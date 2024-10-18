c47ded7d99c8e65e119ef89ff6eee1f3d383c5fb
Kendrick E. Igbinosun <kendrickigb@outlook.com>
2024-10-18 12:08:35 +0100

    #

---
+++ MyParseTest.java
@@ -0,0 +1,21 @@
+import java.time.Period;
+import java.util.Scanner;
+
+public class MyParseTest {
+    public static void main(String[] args){
+    Scanner userInput = new Scanner(System.in);
+
+    System.out.println("Enter the years: ");
+    String years = userInput.nextLine();
+    System.out.println("Enter the months: ");
+    String months = userInput.nextLine();
+    System.out.println("Enter the days: ");
+    String days = userInput.nextLine();
+    
+    String time = "P"+ years +"Y"+ months +"M"+ days +"D";
+
+    Period p = Period.parse(time);
+    System.out.println(p.getYears() + " Years " + p.getMonths() + " Months " + p.getDays() + " Days"); 
+
+    }
+}
